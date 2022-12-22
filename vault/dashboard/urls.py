a9e67156ed53e2bbab8339b4d9f3bb19a35f8e3b
Emiliano Spada <emilianos13@gmail.com>
2022-12-23 00:38:51 +0100

    template adjustments

--- vault/dashboard/urls.py
+++ vault/dashboard/urls.py
@@ -1,13 +1,10 @@
 from django.urls import path
-from django.views.generic import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
 
 from . import views
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='index.html'), name="index"),
-    # path("getData/", views.getData, name="getData"),
-    # path('resume/', TemplateView.as_view(template_name='resume.html'), name="resume"),
-    # path('errors/', views.error_list, name="errors")
+    path("", views.home, name="dashboard"),
+    path("settings/", views.settings, name="settings")
 ]
