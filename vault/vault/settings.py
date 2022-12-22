2c78f0f52004651f91fc41870566f9494d273f05
Emiliano Spada <emilianos13@gmail.com>
2022-12-22 23:35:16 +0100

    template first setup

--- vault/vault/settings.py
+++ vault/vault/settings.py
@@ -3,6 +3,7 @@ import os
 from dotenv import load_dotenv
 
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
+DEBUG = True
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -59,14 +60,14 @@ WSGI_APPLICATION = 'vault.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-'''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-'''
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
