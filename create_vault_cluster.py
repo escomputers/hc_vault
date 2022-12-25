7204a73f0b0c1cfa583086ef66a4133f3b949836
Emiliano Spada <emilianos13@gmail.com>
2022-12-25 19:03:08 +0100

    vault environment as code

--- create_vault_cluster.py
+++ create_vault_cluster.py
@@ -1,6 +1,22 @@
 import docker
+import requests
+import time
 
 docker_client = docker.from_env()
-#i = 1
-#for i in range(1,15):
-docker_client.containers.run("--cap-add=IPC_LOCK -e 'VAULT_LOCAL_CONFIG={"storage": {"file": {"path": "/vault/file"}}, "listener": [{"tcp": { "address": "0.0.0.0:8201", "tls_disable": true}}], "default_lease_ttl": "168h", "max_lease_ttl": "720h", "ui": true}' -p 8201:8201 vault server")
+port = 8200
+for i in range(1,3):
+    port += 1
+    vault_config = {'VAULT_LOCAL_CONFIG': '{"storage": {"file": {"path": "/vault/file"}}, "listener": [{"tcp": {"address": "0.0.0.0:' + str(port) + '",' + ' "tls_disable": "true"}}], "default_lease_ttl": "168h", "max_lease_ttl": "720h", "ui": "true"}'}
+
+    docker_client.containers.run('vault', name="vault"+ str(i), command='server', environment=vault_config, cap_add=['IPC_LOCK'], ports={str(port)+'/tcp': port}, detach=True)
+    VAULT_ADDR = 'http://127.0.0.1:' + str(port)
+
+    url = VAULT_ADDR + '/v1/sys/init'
+    ploads = {"secret_shares": 1, "secret_threshold": 1}
+    time.sleep(10)
+    r = requests.post(url, json = ploads)
+
+    file_content = r.text + r.url + '\n'
+    with open('vault_data.txt', 'a') as f:
+        f.write(file_content)
+        f.close()
