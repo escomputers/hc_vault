dc8aa677ac0849dd81cd2b3689a2bfbed88d1bcd
emilianospada <emilianos13@gmail.com>
2022-12-30 20:00:59 +0100

    changed main navigation

--- vault/dashboard/models.py
+++ vault/dashboard/models.py
@@ -1,14 +1,15 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator 
 
 # model for vault entites
 class Node(models.Model):
     vault_node = models.CharField(max_length=100, blank=True, null=True, unique=True)
     alert = models.BooleanField(default=False)
+    threshold = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(999999)], blank=True, null=True)
     email = models.EmailField(blank=True, null=True, unique=True)
     cc = models.EmailField(blank=True, null=True, unique=True)
     cc2 = models.EmailField(blank=True, null=True, unique=True)
     cc3 = models.EmailField(blank=True, null=True, unique=True)
-
 
     def __str__(self):
         return str(self.vault_cluster)
