dd70652f34f02fceb8849195d791a01d166f8e42
emilianospada <emilianos13@gmail.com>
2022-12-31 19:57:30 +0100

    add cluster form implementation

--- vault/dashboard/views.py
+++ vault/dashboard/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
+from .forms import ClusterForm
+from .models import Cluster, Job
+
 
 def home(request):
     return render(request, 'home.html')
+
+
 def clusters(request):
-    return render(request, 'clusters.html')
+    try:
+        clusters = Cluster.objects.all()
+    except Cluster.DoesNotExist:
+        clusters = 'No clusters'
+
+    form = ClusterForm(request.POST)
+    if form.is_valid():
+        form.save()
+    return render(request, 'clusters.html', context={'form': ClusterForm(), 'clusters': clusters})
+
+
 def jobs(request):
     return render(request, 'jobs.html')
