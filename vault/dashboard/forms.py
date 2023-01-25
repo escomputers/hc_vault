df7e26b02b9491284a0d85729fb75ac2a8a072f6
Emiliano Spada <emilianos13@gmail.com>
2023-01-25 14:54:57 +0100

    cluster form as formset with remove button alpha version

--- vault/dashboard/forms.py
+++ vault/dashboard/forms.py
@@ -1,15 +1,17 @@
 from .models import Cluster, Node, Job
-from django.forms import ModelForm, formset_factory
+from django.forms import ModelForm, formset_factory, TextInput
+from django import forms
 
 class ClusterForm(ModelForm):
-	class Meta:
-		model = Cluster
-		fields = ['cluster_name']
+    cluster_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    class Meta:
+        model = Cluster
+        fields = ['cluster_name']
 # use the formset factory to create the formset class
 ClusterFormSet = formset_factory(ClusterForm, extra=1)
 
 class NodeForm(ModelForm):
-	class Meta:
-		model = Node
-		fields = ['url', 'cluster']
+    class Meta:
+        model = Node
+        fields = ['url', 'cluster']
 
